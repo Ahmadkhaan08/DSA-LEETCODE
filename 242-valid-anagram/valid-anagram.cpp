8845e373e62e4183201bd70d8a094afe912b7ea7
Mohammad Ahmad Khan <160271387+Ahmadkhaan08@users.noreply.github.com>
2025-12-24 09:42:52 +0500

    Time: 1 ms (56.84%) | Memory: 9.7 MB (73.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> shash(26);
-        vector<int> thash(26);
+        vector<int> hm(26);
         if (s.length() != t.length())
             return false;
         for (int i = 0; i < s.length(); i++) {
-            shash[s[i] - 'a']++;
-            thash[t[i] - 'a']++;
+            hm[s[i] - 'a']++;
+            hm[t[i] - 'a']--;
         }
-
-        return equal(shash.begin(), shash.end(), thash.begin());
+        for (int i : hm) {
+            if (i != 0)
+                return false;
+        }
+        return true;
     }
 };
